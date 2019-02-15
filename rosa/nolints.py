0c5eeede15dbb10503b2e1b03138d748d4f3a762
Russell Hyde <russ_hyde@hotmail.com>
2019-02-15 12:14:06 +0000

    black/pylint passing version

---
+++ rosa/nolints.py
@@ -0,0 +1,54 @@
+"""
+Script to compute the `longest increasing subsequence`
+"""
+
+STREAM = open("data_LGIS.txt", "r").read().splitlines()
+
+MAXNUM = int(STREAM[0])
+PERM = [int(x) for x in STREAM[1].split()]
+
+
+def lgis(perm):
+    """
+    Obtain the longest increasing subsequence from a list of integers
+    """
+    #
+    levels = []
+    graph = {}
+    for i in perm:
+        if not levels:
+            levels.append([i])
+            graph[i] = 0
+        else:
+            # append to the highest level where i is greater than some
+            # value in the next lowest level
+            for lev in reversed(range(len(levels) + 1)):
+                if i in graph.keys():
+                    break
+                if lev == 0:
+                    levels[0].append(i)
+                    graph[i] = 0
+                    break
+                lower_lev = levels[lev - 1]
+                parents = [x for x in lower_lev if x < i]
+                if parents:
+                    if len(levels) == lev:
+                        levels.append([i])
+                    else:
+                        levels[lev].append(i)
+                        # drop entries in this level if they are greater than i
+                        lev_gt = [x for x in levels[lev] if x > i]
+                        levels[lev] = list(set(levels[lev]).difference(set(lev_gt)))
+                    graph[i] = parents[0]
+    # returnable data
+    res = []
+    i = levels[-1][0]
+    while i != 0:
+        res.append(i)
+        i = graph[i]
+    res = list(reversed(res))
+    return res
+
+
+print(" ".join([str(x) for x in lgis(PERM)]))
+print(" ".join(list(reversed([str(x) for x in lgis(list(reversed(PERM)))]))))
